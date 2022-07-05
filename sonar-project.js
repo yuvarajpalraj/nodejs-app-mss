cbe5e701d8f5f3f803ec99bd2461f87275d8c629
yuvarajpalraj <107245371+yuvarajpalraj@users.noreply.github.com>
2022-07-05 13:52:37 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://15.206.172.213:9000/',
+  serverUrl: 'http://3.110.179.244:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
